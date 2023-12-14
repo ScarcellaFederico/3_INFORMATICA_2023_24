5304da91f217fb6097cd62c9404e3c1c150afeac
Federico <Federico.scarcella2006@gmail.com>
2023-12-14 16:13:24 +0100

    prova del primo commit

---
+++ Es.java
@@ -0,0 +1,13 @@
+
+public class Es{
+    public static void main(String[] args) {
+        int a,b,r;
+        a = 1;
+        b = 3;
+
+        r = a+b;
+
+        System.out.println(r);
+
+    }
+}
